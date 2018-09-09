d2fb0104cc87909fa58aad3ab347a7374f8142ef
Oleh1033 <oleg103356@gmail.com>
2018-09-09 12:43:15 +0200

    finish

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
 $(window).scroll(function () {
     var w$scroll =  $(this).scrollTop();
 
-    if(w$scroll>0&&w$scroll<205){
-        $('.layer-0').css({'transform' : 'translate(0px, '+ w$scroll*-1+'px)'});
-        $('.layer-2').css({'transform' : 'translate(0px,'+ w$scroll*0.5+'px)'})
-        $('.layer-3').css({'transform' : 'translate(0px,'+ w$scroll*0.5+'px)'})
-        $('.layer-4').css({'transform' : 'translate(0px,'+ w$scroll*1+'px)'})
-        $('.layer-5').css({'transform' : 'translate(0px,'+ w$scroll*1.5+'px)'})
+    if (!(w$scroll > 0 && w$scroll < 205)) {
+    } else {
+        $('.layer-1').css({'transform' : 'rotate3d(1, 0, 0,' + w$scroll/4 +  'deg)'})
+
+        $('.layer-0').css({'opacity':w$scroll/200})
+        $('.layer-2').css({'opacity':w$scroll/200})
+        $('.layer-3').css({'opacity':w$scroll/200})
+        $('.layer-4').css({'opacity':w$scroll/200})
+        $('.layer-5').css({'opacity':w$scroll/200})
+
+
+        $('.layer-0').css({'transform': 'translate3d(0px, ' + w$scroll * -0.5 + 'px, 0px) rotate3d(1, 0, 0,' + w$scroll/4 + 'deg)'})
+        $('.layer-2').css({'transform': 'translate3d(0px,' + w$scroll * 0.5 + 'px, 0px) rotate3d(1, 0, 0,' + w$scroll/4 + 'deg)'})
+        $('.layer-3').css({'transform': 'translate3d(0px,' + w$scroll * 0.7 + 'px, 0px)rotate3d(1, 0, 0,' + w$scroll/4 + 'deg)'})
+        $('.layer-4').css({'transform': 'translate3d(0px,' + w$scroll * 1.3 + 'px, 0px)rotate3d(1, 0, 0,' + w$scroll/4 + 'deg)'})
+        $('.layer-5').css({'transform': 'translate3d(0px,' + w$scroll * 1.8 + 'px, 0px)rotate3d(1, 0, 0,' + w$scroll/4 + 'deg)'})
     }
 
-    if(w$scroll>0&&w$scroll<250){$('.layer-0').css({'opacity':w$scroll/200})}
-    if(w$scroll>0&&w$scroll<250){$('.layer-2').css({'opacity':w$scroll/200})}
-    if(w$scroll>0&&w$scroll<250){$('.layer-3').css({'opacity':w$scroll/200})}
-    if(w$scroll>0&&w$scroll<250){$('.layer-4').css({'opacity':w$scroll/200})}
-    if(w$scroll>0&&w$scroll<250){$('.layer-5').css({'opacity':w$scroll/200})}
-
-    if(w$scroll>0&&w$scroll<205) {
-        $('.layer-1').css({'transform' : 'rotate3d(1, 0, 0,' + w$scroll/3.9 +  'deg)'});
-    }
+    if(w$scroll>0&&w$scroll<250){}
+    if(w$scroll>0&&w$scroll<250){}
+    if(w$scroll>0&&w$scroll<250){}
+    if(w$scroll>0&&w$scroll<250){}
+    if(w$scroll>0&&w$scroll<250){}
 
 });
