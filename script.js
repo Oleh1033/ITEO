dacbf72c43fc4348b0265a87a0c3f930ac2550b3
Oleh1033 <oleg103356@gmail.com>
2018-09-09 09:56:48 +0200

    phone

--- script.js
+++ script.js
@@ -1,51 +1,22 @@
 $(window).scroll(function () {
     var w$scroll =  $(this).scrollTop();
 
-    console.log(w$scroll);
-    if(w$scroll>10&&w$scroll<205){
-        $('.layer-0').css({
-            'transform' : 'translate(0px, '+ w$scroll*-1+'px)'
-        });
-
-        $('.layer-2').css({
-            'transform' : 'translate(0px,'+ w$scroll*-0.5+'px)'
-        })
-
-        $('.layer-3').css({
-            'transform' : 'translate(0px,'+ w$scroll*0.5+'px)'
-        })
-        $('.layer-4').css({
-            'transform' : 'translate(0px,'+ w$scroll*1+'px)'
-        })
-        $('.layer-5').css({
-            'transform' : 'translate(0px,'+ w$scroll*1.5+'px)'
-        })
+    if(w$scroll>0&&w$scroll<205){
+        $('.layer-0').css({'transform' : 'translate(0px, '+ w$scroll*-1+'px)'});
+        $('.layer-2').css({'transform' : 'translate(0px,'+ w$scroll*0.5+'px)'})
+        $('.layer-3').css({'transform' : 'translate(0px,'+ w$scroll*0.5+'px)'})
+        $('.layer-4').css({'transform' : 'translate(0px,'+ w$scroll*1+'px)'})
+        $('.layer-5').css({'transform' : 'translate(0px,'+ w$scroll*1.5+'px)'})
     }
 
-    if(w$scroll>10&&w$scroll<250){
-        var opacity = w$scroll/200;
-        $('.layer-0').css({'opacity':opacity})
-    }
- /*   if(w$scroll>10){
-        var opacity = 1 - w$scroll/400;
-        $('.layer-1').css({'opacity':opacity})
-    }
-   */
-    if(w$scroll>10&&w$scroll<250){
-        var opacity = w$scroll/200;
-        $('.layer-2').css({'opacity':opacity})
-    }
-    if(w$scroll>10&&w$scroll<250){
-        var opacity = w$scroll/200;
-        $('.layer-3').css({'opacity':opacity})
-    }
-    if(w$scroll>10&&w$scroll<250){
-        var opacity = w$scroll/200;
-        $('.layer-4').css({'opacity':opacity})
-    }
-    if(w$scroll>10&&w$scroll<250){
-        var opacity = w$scroll/200;
-        $('.layer-5').css({'opacity':opacity})
+    if(w$scroll>0&&w$scroll<250){$('.layer-0').css({'opacity':w$scroll/200})}
+    if(w$scroll>0&&w$scroll<250){$('.layer-2').css({'opacity':w$scroll/200})}
+    if(w$scroll>0&&w$scroll<250){$('.layer-3').css({'opacity':w$scroll/200})}
+    if(w$scroll>0&&w$scroll<250){$('.layer-4').css({'opacity':w$scroll/200})}
+    if(w$scroll>0&&w$scroll<250){$('.layer-5').css({'opacity':w$scroll/200})}
+
+    if(w$scroll>0&&w$scroll<205) {
+        $('.layer-1').css({'transform' : 'rotate3d(1, 0, 0,' + w$scroll/3.9 +  'deg)'});
     }
 
 });
