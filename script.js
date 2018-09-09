4d297cf554ca9891b798664e161be730ae85ae7c
Oleh1033 <oleg103356@gmail.com>
2018-09-09 12:48:26 +0200

    last

--- script.js
+++ script.js
@@ -11,18 +11,10 @@ $(window).scroll(function () {
         $('.layer-4').css({'opacity':w$scroll/200})
         $('.layer-5').css({'opacity':w$scroll/200})
 
-
-        $('.layer-0').css({'transform': 'translate3d(0px, ' + w$scroll * -0.5 + 'px, 0px) rotate3d(1, 0, 0,' + w$scroll/4 + 'deg)'})
-        $('.layer-2').css({'transform': 'translate3d(0px,' + w$scroll * 0.5 + 'px, 0px) rotate3d(1, 0, 0,' + w$scroll/4 + 'deg)'})
-        $('.layer-3').css({'transform': 'translate3d(0px,' + w$scroll * 0.7 + 'px, 0px)rotate3d(1, 0, 0,' + w$scroll/4 + 'deg)'})
-        $('.layer-4').css({'transform': 'translate3d(0px,' + w$scroll * 1.3 + 'px, 0px)rotate3d(1, 0, 0,' + w$scroll/4 + 'deg)'})
-        $('.layer-5').css({'transform': 'translate3d(0px,' + w$scroll * 1.8 + 'px, 0px)rotate3d(1, 0, 0,' + w$scroll/4 + 'deg)'})
+        $('.layer-0').css({'transform': 'translate3d(0px,' + w$scroll * -0.5 + 'px, 0px) rotate3d(1, 0, 0,' + w$scroll/4 + 'deg)'})
+        $('.layer-2').css({'transform': 'translate3d(0px,' + w$scroll * 0.5 + 'px, 0px)  rotate3d(1, 0, 0,' + w$scroll/4 + 'deg)'})
+        $('.layer-3').css({'transform': 'translate3d(0px,' + w$scroll * 0.7 + 'px, 0px)  rotate3d(1, 0, 0,' + w$scroll/4 + 'deg)'})
+        $('.layer-4').css({'transform': 'translate3d(0px,' + w$scroll * 1.3 + 'px, 0px)  rotate3d(1, 0, 0,' + w$scroll/4 + 'deg)'})
+        $('.layer-5').css({'transform': 'translate3d(0px,' + w$scroll * 1.8 + 'px, 0px)  rotate3d(1, 0, 0,' + w$scroll/4 + 'deg)'})
     }
-
-    if(w$scroll>0&&w$scroll<250){}
-    if(w$scroll>0&&w$scroll<250){}
-    if(w$scroll>0&&w$scroll<250){}
-    if(w$scroll>0&&w$scroll<250){}
-    if(w$scroll>0&&w$scroll<250){}
-
 });
